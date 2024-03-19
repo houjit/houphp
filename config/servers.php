62bf7e0ca46a123d48835851a11c9fc209c48685
17612115819 <amos@houjit.com>
2024-03-19 17:54:02 +0800

    amos

--- config/servers.php
+++ config/servers.php
@@ -25,7 +25,6 @@ return [
             'worker_num'            => swoole_cpu_num(),
             'task_worker_num'       => swoole_cpu_num(),
             'enable_static_handler' => true,
-            'document_root'         => root_path('public'),
             'package_max_length'    => 20 * 1024 * 1024,
             'buffer_output_size'    => 10 * 1024 * 1024,
             'socket_buffer_size'    => 128 * 1024 * 1024,
